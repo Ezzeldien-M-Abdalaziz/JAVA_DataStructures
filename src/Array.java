93cc4cf8e96bf70499e7e115aabb7330d3a5e6be
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2025-06-18 18:52:05 +0300

    Array methods

--- src/Array.java
+++ src/Array.java
@@ -1,2 +1,47 @@
 public class Array {
+    private int [] items;
+    private int count; //number of items in the array
+
+    Array(int size){
+        items = new int[size];   //initialize the array with the size
+        count = 0;    //number of elements are zero
+    }
+
+    //add to the end of the array
+    void append(int item){
+        //check if there is room in the array
+        if(count == items.length){
+            System.out.println("Array is full , cannot add anymore items");
+            return;
+        }else{
+            items[count] = item; //insert in the end of the array since count is the real number of elements +1
+            count++;  // increase the count
+        }
+    }
+
+    //get alll items
+    void traverse(){
+        for(int i = 0; i<count; i++){
+            System.out.println(items[i]);
+        }
+    }
+
+    //search for an item
+    boolean search(int item){
+        for(int i = 0; i<count; i++){
+            if(items[i] == item){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //getter count
+    public int getCount(){
+        return count;
+    }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
-
     public static void main(String[] args) {
+
+        //**********************************************Array********************************************
+        Array arr = new Array(5);
+        arr.append(1);
+        arr.append(2);
+        arr.append(5);
+        arr.traverse();
+        System.out.println(arr.search(3));
+        System.out.println(arr.search(1));
+        arr.append(2);
+        arr.append(2);
+        arr.append(2);
+        arr.traverse();
+        System.out.println(arr.getCount());
+
+
+        //**********************************************LinkedList********************************************
         LinkedList list = new LinkedList();
         list.insertLast(10);
         list.insertLast(20);
